d90d6de24aaa1742fce3cb34c91482ed739b617e
unknown <ikoaymen056@gmail.com>
2020-12-31 13:50:22 +0100

    'My1EtWebsite'

---
+++ worbm.js
@@ -0,0 +1,21 @@
+// Setup
+function phoneticLookup(val) {
+    var result =val;
+  
+    // Only change code below this line
+    var result={
+        "alpha": "Adams",
+         "bravo": "Boston",
+         "charlie": "Chicago",
+         "delta":"Denver", 
+         "echo":"Easy", 
+         "foxtrot": "Frank",
+
+    }
+    result.val;
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  phoneticLookup("charlie");
